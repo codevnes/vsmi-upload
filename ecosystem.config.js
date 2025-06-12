4d31eb67ac271234e06e48815d9cdd7a56199dac
Tran Danh Trong <codevnes@gmail.com>
2025-06-12 19:50:16 +0700

    Update Thu Jun 12 19:50:16 +07 2025

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,14 @@ module.exports = {
   apps: [
     {
       name: "vsmi-upload",
-      script: "./dist/index.js",
+      script: "dist/index.js",
+      cwd: __dirname,
       env: {
         NODE_ENV: "production",
         PORT: 2025
+      },
+      env_production: {
+        NODE_ENV: "production"
       },
       instances: 1,
       autorestart: true,
