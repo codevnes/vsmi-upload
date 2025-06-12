1994559ae70115ee77feb21d158b0423a2f62cc9
Tran Danh Trong <codevnes@gmail.com>
2025-06-12 19:32:12 +0700

    Update Thu Jun 12 19:32:12 +07 2025

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,1 +1,17 @@
- 
+module.exports = {
+  apps: [
+    {
+      name: "vsmi-upload",
+      script: "./dist/index.js",
+      env: {
+        NODE_ENV: "production",
+        PORT: 2025
+      },
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      log_date_format: "YYYY-MM-DD HH:mm:ss Z"
+    }
+  ]
+}; 
